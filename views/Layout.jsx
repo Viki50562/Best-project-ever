ffbbed1f0ad290eb9ac68c9a779a8f815f56e7d5
Anton Gorozhankin <obrv.igl@gmail.com>
2022-07-27 19:32:37 +0300

    Config + App Setup

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,23 +1,24 @@
 const React = require('react');
 
-function Layout({title, children}){
-    return(
+module.exports = function Layout({ title, children }) {
+  return (
     <html lang="en">
-    <head>
+      <head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-    </head>
-    <body>
-    <header class="header" >
-        <h1 class="logo">Dosta-v-O4ko!</h1> 
-        <div class="auth">
-            <button type="button" class="btn btn-dark ">Rega</button>
-            <button type="button" class="btn btn-dark">Loga</button>
-        </div>
-    </header>
+      </head>
+      <body>
+        <header className="header">
+          <h1 className="logo">Dosta-v-O4ko!</h1>
+          <div className="auth">
+            <button type="button" className="btn btn-dark ">Rega</button>
+            <button type="button" className="btn btn-dark">Loga</button>
+          </div>
+        </header>
         {children}
-    </body>
-    </html>)
+      </body>
+    </html>
+  );
 }
