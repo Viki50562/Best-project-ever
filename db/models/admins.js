f1a1ca853a2327150df48e2933820b72c73f3400
Viktoria <syrcova.viktorija@gmail.com>
2022-07-27 18:37:11 +0300

    Database done

---
+++ db/models/admins.js
@@ -0,0 +1,25 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class admins extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  admins.init({
+    name: DataTypes.TEXT,
+    email: DataTypes.TEXT,
+    password: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'admins',
+  });
+  return admins;
+};
